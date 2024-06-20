349037181f148a964d380a60851ba1015efc53da
Shruti Gupta <strength.shruti@gmail.com>
2024-06-20 23:31:55 +0530

    Delete yourstoryHack/gungun directory

--- yourstoryHack/gungun/weather/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import WeatherData
-
-class WeatherDataForm(forms.ModelForm):
-    class Meta:
-        model = WeatherData
-        fields = ['date', 'temperature', 'humidity', 'rainfall']
